0ff21801399d757824c3d3f1be34881a7e928b97
Özgür SARIKAMIS <ozgursarikamis@gmail.com>
2020-08-16 00:33:28 +0300

    splash_content.dart

--- lib/screens/splash/components/body.dart
+++ lib/screens/splash/components/body.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-
-import '../../../constants.dart';
-import '../../../size_config.dart';
+import 'splash_content.dart';
 
 class Body extends StatefulWidget {
   Body({Key key}) : super(key: key);
@@ -50,33 +48,6 @@ class _BodyState extends State<Body> {
           ],
         ),
       ),
-    );
-  }
-}
-
-class SplashContent extends StatelessWidget {
-  const SplashContent({Key key, this.text, this.image}) : super(key: key);
-
-  final String text, image;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        Spacer(),
-        Text("TOKOTO",
-            style: TextStyle(
-                fontSize: getProportionateScreenWidth(36),
-                color: kPrimaryColor,
-                fontWeight: FontWeight.bold)),
-        Text(text),
-        Spacer(),
-        Image.asset(
-          image,
-          height: getProportionateScreenHeight(265),
-          width: getProportionateScreenWidth(235),
-        )
-      ],
     );
   }
 }
